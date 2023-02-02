72f9951631d8fcc4dd09e935d77d8ec77a58d674
HellCAT0147 <letscheckmail23@gmail.com>
2023-02-02 20:04:14 +0500

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix) return [];
+  let layer = false;
+  const towels = [];
+  for (row of matrix) {
+    if (layer) row = row.reverse();
+    for (sym of row) {
+      towels.push(sym);
+    }
+    layer = !layer;
+  }
+  return towels;
 }
